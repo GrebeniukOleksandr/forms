ca25174d549e7e7be9171f487a46c280a4abb4a7
Oleksandr <sashkagrebe@gmail.com>
2023-10-27 15:29:34 +0300

    open button

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,13 @@
 formFixed.style.right = window.innerWidth - wrapper.getBoundingClientRect().right + "px"
 
 let clikClose = document.getElementById("close")
-    flexClose = document.getElementById("formFixed")
-clikClose.addEventListener('click' , function() {
-     flexClose.style.display = 'none';
+flexClose = document.getElementById("formFixed")
+clikOpen = document.getElementById("open-block")
+clikClose.addEventListener('click', function () {
+    flexClose.style.display = 'none'
+    clikOpen.style.display = 'block'
+})
+clikOpen.addEventListener('click', function () {
+    clikOpen.style.display = 'none'
+    flexClose.style.display = 'block'
 })
