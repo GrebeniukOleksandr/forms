85047c7eb9cdb9fb2ab6cc960c1e73e17398dd2a
Oleksandr <sashkagrebe@gmail.com>
2023-10-28 16:08:18 +0300

    add open button

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,18 @@
 formFixed.style.right = window.innerWidth - wrapper.getBoundingClientRect().right + "px"
 
-let clikClose = document.getElementById("close")
-flexClose = document.getElementById("formFixed")
-clikOpen = document.getElementById("open-block")
-clikClose.addEventListener('click', function () {
-    flexClose.style.display = 'none'
-    clikOpen.style.display = 'block'
-})
-clikOpen.addEventListener('click', function () {
-    clikOpen.style.display = 'none'
-    flexClose.style.display = 'block'
-})
+let clikClose = document.getElementById("close");
+ flexClose = document.getElementById("formFixed");
+ clikOpen = document.getElementById("open-block");
+
+function toggleDisplay() {
+    if (flexClose.style.display === 'none') {
+        flexClose.style.display = 'block';
+        clikOpen.style.display = 'none';
+    } else {
+        flexClose.style.display = 'none';
+        clikOpen.style.display = 'block';
+    }
+}
+
+clikClose.addEventListener('click', toggleDisplay);
+clikOpen.addEventListener('click', toggleDisplay);
